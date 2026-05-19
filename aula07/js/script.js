b3040080f24da70881c2d5c43663010e1927c426
franciscodouglas <98854868+franciscodouglas@users.noreply.github.com>
2026-05-19 10:14:35 -0300

    removendo pasta aula10

--- aula07/js/script.js
+++ aula07/js/script.js
@@ -3,7 +3,6 @@ document.getElementById("btnConfirma").addEventListener('click', function(){
 
     let email = document.getElementById("email").value
     let password = document.getElementById("password").value
-
     alert('Login Realizado: '+email+" "+password)
     console.log(email,password)
 
